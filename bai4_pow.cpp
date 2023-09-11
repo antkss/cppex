0b02cc88336da966b9a22b54f24a66f74c9f38c3
antkss <anfixgapps@gmail.com>
2023-09-11 16:47:00 +0700

    pow

---
+++ bai4_pow.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int x,y;
+    cin >>x>>y;
+    long long int l = (long long int)pow(x,y);
+    cout<<l;
+
+    return 0;
+}
+
